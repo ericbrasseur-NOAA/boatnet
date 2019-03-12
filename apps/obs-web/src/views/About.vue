86f8bbc50122a2021f71d1e006bc1a95ab7b8c0f
Will Smith <will.smith@noaa.gov>
2019-03-12 13:20:18 -0700

    Refactor About.vue

--- apps/obs-web/src/views/About.vue
+++ apps/obs-web/src/views/About.vue
@@ -1,11 +1,12 @@
 <template>
   <q-page padding>
-    <p>This is an about page</p>
+    <p>This is an About page.</p>
   </q-page>
 </template>
 
-<script>
-export default {
-  name: 'PageAbout'
-};
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class PageAbout extends Vue {}
 </script>
