b151bcc20f7c464d5c3e0d6bee23f5b5970bd62e
Todd Hay <todd.hay@noaa.gov>
2019-03-18 09:31:48 -0700

    Update tasks/options for Observer-specific elements

--- apps/obs-web/src/views/Chart.vue
+++ apps/obs-web/src/views/Chart.vue
@@ -14,24 +14,23 @@ import dayjs from "dayjs";
 const tasks2 = [
   {
     id: 1,
-    label: 'Make some noise',
+    label: 'Shawn Rogers',
     user:
       '<a href="https://www.google.com/search?q=John+Doe" target="_blank" style="color:#0077c0;">John Doe</a>',
-    start: getDate(-24 * 5),
+    start: getDate(-22 * 5),
     duration: 5 * 24 * 60 * 60,
     progress: 85,
-    type: 'project'
+    type: 'task' //'project'
   },
   {
     id: 2,
-    label: 'With great power comes great responsibility',
+    label: 'Salvador Ramirez',
     user:
       '<a href="https://www.google.com/search?q=Peter+Parker" target="_blank" style="color:#0077c0;">Peter Parker</a>',
-    parentId: 1,
     start: getDate(-24 * 4),
     duration: 4 * 24 * 60 * 60,
     progress: 50,
-    type: 'milestone',
+    type: 'task', // 'milestone',
     style: {
       base: {
         fill: '#1EBC61',
@@ -48,10 +47,10 @@ const tasks2 = [
   },
   {
     id: 3,
-    label: 'Courage is being scared to death, but saddling up anyway.',
+    label: 'Dylan Tauzer',
     user:
       '<a href="https://www.google.com/search?q=John+Wayne" target="_blank" style="color:#0077c0;">John Wayne</a>',
-    parentId: 2,
+    // parentId: 2,
     start: getDate(-24 * 3),
     duration: 2 * 24 * 60 * 60,
     progress: 100,
@@ -62,31 +61,31 @@ const tasks2 = [
 
 const options2 = {
   title: {
-    label: 'Your project title as html',
+    label: 'Observer Activity Tracking (OAT)',
     html: false
   },
   taskList: {
     columns: [
       { id: 1, label: 'ID', value: 'id', width: 40 },
-      { id: 2, label: 'Description', value: 'label', width: 200, expander: true },
-      { id: 3, label: 'Assigned to', value: 'user', width: 130, html: true },
+      { id: 2, label: 'Observer', value: 'label', width: 200, expander: true },
+      { id: 3, label: 'Debriefer', value: 'user', width: 130, html: true },
       { id: 3, label: 'Start', value: (task: any) => dayjs(task.start).format('YYYY-MM-DD'), width: 78 },
-      { id: 4, label: 'Type', value: 'type', width: 68 },
-      {
-        id: 5,
-        label: '%',
-        value: 'progress',
-        width: 35,
-        style: {
-          'task-list-header-label': {
-            'text-align': 'center',
-            width: '100%'
-          },
-          'task-list-item-value-container': {
-            'text-align': 'center'
-          }
-        }
-      }
+      { id: 4, label: 'Trip Status', value: 'type', width: 68 },
+      // {
+      //   id: 5,
+      //   label: '%',
+      //   value: 'progress',
+      //   width: 35,
+      //   style: {
+      //     'task-list-header-label': {
+      //       'text-align': 'center',
+      //       width: '100%'
+      //     },
+      //     'task-list-item-value-container': {
+      //       'text-align': 'center'
+      //     }
+      //   }
+      // }
     ]
   },
   locale: {
